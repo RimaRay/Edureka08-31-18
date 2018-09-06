9fe29d2ec7bc5f6cb7565acfa6de69c230fce710
RimaRay <42882284+RimaRay@users.noreply.github.com>
2018-09-05 20:28:25 -0400

    Updated hello world

--- EdurekaProject082518/src/day1/HelloWorld.java
+++ EdurekaProject082518/src/day1/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World Again");
                 
-		System.out.println("Hello World Again3")
+		System.out.println("Hello World Again3");
 	
 		System.out.println("Hello World Again2");                            
 	}
